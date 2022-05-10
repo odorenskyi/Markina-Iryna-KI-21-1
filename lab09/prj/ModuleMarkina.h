2b07d0808ea34ab3ba566a55231ed506f726f5d1
Markina Iryna <98706839+mastefi@users.noreply.github.com>
2022-05-10 22:20:22 +0300

    Add files via upload

---
+++ lab09/prj/ModuleMarkina.h
@@ -0,0 +1,15 @@
+#ifndef ModulesMarkina.h
+#include <math.h>
+#include <bitset>
+#define ModulesMarkina.h
+#define _USE_MATH_DEFINES
+
+bool logvir(float a, float b)
+void DecHex(int x, int y, int z)
+double s_calculation(double x, double y, double z)
+void viter(int *n,double *v1,double *v2);
+double roz(double cm);
+int bin(int *nb,int *vv,int *res);
+double s_calculation(double x, double y, double z);
+
+#endif
